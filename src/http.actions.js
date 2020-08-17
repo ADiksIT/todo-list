7eff7ef876d98b0c16f38046101aebdbb257458a
ADiks <antondikson@gmail.com>
2020-08-17 10:08:16 +0300

    fix http request

--- src/http.actions.js
+++ src/http.actions.js
@@ -1,17 +1,17 @@
 export const apiReplace = (id, start, end) =>
-  `/api/todos/users/${id}/replace/start/${start}/end/${end}`;
+  `https://todo1111112.herokuapp.com/api/todos/users/${id}/replace/start/${start}/end/${end}`;
 
-export const apiAddTodo = (id) => `/api/todos/users/${id}`;
+export const apiAddTodo = (id) => `https://todo1111112.herokuapp.com/api/todos/users/${id}`;
 
 export const apiChangeTextTodo = (user, todo) =>
-  `/api/todos/users/${user}/change/text/${todo}`;
+  `https://todo1111112.herokuapp.com/api/todos/users/${user}/change/text/${todo}`;
 
 export const apiChangeCompletedTodo = (user, todo) =>
-  `/api/todos/users/${user}/change/completed/${todo}`;
+  `https://todo1111112.herokuapp.com/api/todos/users/${user}/change/completed/${todo}`;
 
 export const apiGetTodos = (id) => `/api/todos/users/${id}`;
 
 export const apiDeleteTodo = (user, todo) =>
-  `/api/todos/users/${user}/delete/${todo}`;
+  `https://todo1111112.herokuapp.com/api/todos/users/${user}/delete/${todo}`;
 
-export const apiRegisterUser = () => '/api/auth/register_login';
+export const apiRegisterUser = () => 'https://todo1111112.herokuapp.com/api/auth/register_login';
